4a1bf5b06598dd53556266fd3b459161a25a70e1
dinho11701 <dinho11701@Virtual.myguest.virtualbox.org>
2024-05-07 23:11:19 -0400

    add fisrt speech for Alexa

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # AlexaBook
+
+#pip install pyttsx3
+#pip install PyPDF2
+#sudo dpkg --configure -a
+
+#sudo apt-get install espeak
+#locate libespeak.so
---
+++ main.py
@@ -0,0 +1,5 @@
+import pyttsx3
+
+droid = pyttsx3.init()
+droid.say('Bonjour Oswald, je suis un droid de la 3e génération et je suis Alexa')
+droid.runAndWait()
